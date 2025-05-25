acf80bba1b86bf93602908ed426f237831fdfeb1
John Adib <mr.john.adib@gmail.com>
2025-05-25 14:06:29 +0100

    add logo

--- src/components/homepage/hero/index.tsx
+++ src/components/homepage/hero/index.tsx
@@ -1,16 +1,22 @@
 import { UVA2025eventDateFormatted } from "@/src/constants/event-2025";
 import { siteTitle } from "@/src/constants/site";
+import { Logo } from "../../logo";
 
 export const HomepageHero = () => {
 	return (
 		<section className="bg-vision-red">
 			<div className="container mx-auto py-48 text-center">
-				<h1 className="text-white text-9xl font-extrabold text-balance mb-8 leading-none">
-					{siteTitle} 2025
-				</h1>
-				<h2 className="text-white text-4xl font-bold mb-12">
-					{UVA2025eventDateFormatted}
-				</h2>
+				<div>
+					<h1 className="text-white text-9xl font-extrabold text-balance mb-8 leading-none">
+						{siteTitle} 2025
+					</h1>
+					<h2 className="text-white text-4xl font-bold mb-12">
+						{UVA2025eventDateFormatted}
+					</h2>
+				</div>
+				<div>
+					<Logo />
+				</div>
 			</div>
 		</section>
 	);
---
+++ src/components/logo/index.tsx
@@ -0,0 +1,8 @@
+export const Logo = () => {
+	return (
+		<div className="logo">
+			<div className="text-6xl">🦄</div>
+			<h1>Unicorn Vision Awards</h1>
+		</div>
+	);
+};
